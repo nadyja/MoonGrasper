6a4f5f50138f9ed9bddc16233dcb95df5e0c1162
nmietkiewicz <nadia.mietkiewicz@pracuj.pl>
2016-04-23 20:49:35 +0200

    better debug

--- www/js/AppCtrl.js
+++ www/js/AppCtrl.js
@@ -11,62 +11,113 @@ angular.module('lunagrab.controllers', []).controller('AppCtrl', function($scope
     $scope.init = function() {
 
         getMoonPosition(52, 14, 2).then(function(result) {
-        	
+
             $scope.moon = {
-                tilt: result[0],
-                compass: result[1]
+                tilt: parseFloat(result[0]),
+                compass: parseFloat(result[1])
             }
-              $scope.moon = {
-        compass: 0,
-        tilt: 90
-    }
-            debug('moon position (tilt,compass): ' + moon.tilt + "   " + moon.compass, 4);
+            debug(0,'moon position (compass, tilt): ',$scope.moon.compass,$scope.moon.tilt);
             updatePosition({
-                tilt: 87,
-                compass: 3,
+                tilt: 75,
+                compass: 144,
                 lat: 0,
                 lng: 0
             });
+            initializeEzar();
+
         })
 
     }
+
+
+    function initializeEzar() {
+    	debug(5,'init');
+
+                debug(5,'no device orientation');
+                if (window.DeviceOrientationEvent) {
+                    debug(5,'device orientation present');
+
+
+                    window.addEventListener('deviceorientation', function(eventData) {
+                        // gamma is the left-to-right tilt in degrees, where right is positive
+                        var tiltLR = Math.floor(eventData.gamma);
+
+                        // beta is the front-to-back tilt in degrees, where front is positive
+                        var tiltFB = Math.floor(eventData.beta);
+
+                        // alpha is the compass direction the device is facing in degrees
+                        var dir = Math.floor(eventData.alpha);
+
+                        // call our orientation event handler
+                        $scope.orientation.tilt = tiltFB;
+						$scope.$apply();
+                        doUpdate();
+                        //debug(tiltLR, tiltFB, dir);
+                    }, false);
+
+
+                }
+
+
+
+                setInterval(function() {
+                    navigator.compass.getCurrentHeading(
+                        function(heading) {
+                            var hdng = Math.floor(heading.magneticHeading);
+                            $scope.orientation.compass = hdng;
+                            $scope.$apply();
+                            doUpdate();
+
+                        },
+                        function(err) {
+                            console.log("Compass error", err);
+                            debug(5,err);
+                            ezar.getBackCamera().start();
+                        });
+                }, 500);
+
+
+            }
+
+
     $scope.init();
-  function getArrowAngle(delta) {
-    var v = delta.v;
-    var h = delta.h;
 
-    if (h === 0 && v === 0) {
-      return null
-    } else if (v === 0) {
-      if (h > 0) {
-        return -90
-      } else {
-        return -270
-      }
-    } else if (h === 0) {
-      if (v > 0) {
-        return 0
-      } else {
-        return -180
-      }
+    function getArrowAngle(delta) {
+        var v = delta.v;
+        var h = delta.h;
+
+        if (h === 0 && v === 0) {
+            return null
+        } else if (v === 0) {
+            if (h > 0) {
+                return -90
+            } else {
+                return -270
+            }
+        } else if (h === 0) {
+            if (v > 0) {
+                return 0
+            } else {
+                return -180
+            }
+        }
+
+        var radians = Math.atan(delta.h / delta.v);
+        var deg = Math.abs(radians * 180 / Math.PI);
+
+        //clockwise rotation
+        if (h > 0 && v > 0) {
+            return -deg
+        } else if (h > 0 && v < 0) {
+            return -(180 - deg)
+        } else if (h < 0 && v < 0) {
+            return -(180 + deg)
+        } else if (h < 0 && v > 0) {
+            return -(270 + deg)
+        }
+
+
     }
-
-    var radians = Math.atan(delta.h / delta.v);
-    var deg = Math.abs(radians * 180 / Math.PI);
-
-    //clockwise rotation
-    if (h > 0 && v > 0) {
-      return -deg
-    } else if (h > 0 && v < 0) {
-      return -(180 - deg)
-    } else if (h < 0 && v < 0) {
-      return -(180 + deg)
-    } else if (h < 0 && v > 0) {
-      return -(270 + deg)
-    }
-
-
-  }
 
     function getDeviceAngleOfView() {
         //mock
@@ -102,21 +153,21 @@ angular.module('lunagrab.controllers', []).controller('AppCtrl', function($scope
         var theta = getDeviceAngleOfView();
         var resolution = getDeviceResolution();
 
-        var pixelsV = resolution.v * delta.v / theta.v;
-        var pixelsH = resolution.h * delta.h / theta.h;
+        var pixelsV = resolution.v * -delta.v / theta.v;
+        var pixelsH = resolution.h * -delta.h / theta.h;
 
         var pixels = {
             v: pixelsV,
             h: pixelsH
         }
-        debug('moon pixels (h, v): ' + pixels.h + "   " + pixels.v, 3);
+        debug(3,'moon pixels (h, v): ', pixels.h, pixels.v);
 
         return pixels;
     }
 
     function getMoonDelta() {
-        diffH = -$scope.orientation.compass + $scope.moon.compass;
-        diffV = $scope.orientation.tilt - $scope.moon.tilt;
+        diffH = $scope.orientation.compass - $scope.moon.compass;
+        diffV = -$scope.orientation.tilt + $scope.moon.tilt;
         return {
             v: diffV,
             h: diffH
@@ -133,36 +184,35 @@ angular.module('lunagrab.controllers', []).controller('AppCtrl', function($scope
 
                 var data = result;
                 var d = new Date();
-                var date = d.getMonth() + "/" + d.getDate() + "/" + d.getFullYear() + "," + d.getHours() + ":00:00";
+                var date = d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear() + "," + d.getHours() + ":00:00";
+                console.log('looking for date  ' + date);
                 data = data.split("\n");
-                console.log('looking for date'+date);
-                console.log(data);
                 for (line in data) {
                     if (data[line].search(date) !== -1) {
-					
+
                         return (data[line].split(",").slice(2));
                     }
                 }
             });
 
-/*
-        var d = new Date();
-        var day = d.getDate();
-        var year = d.getFullYear();
-        var month = d.getMonth();
-        req = 'https://www.nrel.gov/midc/apps/sampa.pl?syear=' + year + '&smonth=' + month + '&sday=' + day + '&eyear=' + year + '&emonth=' + month + '&eday=' + day + '&step=60&stepunit=1&latitude=' + lat + '&longitude=' + lon + '&timezone=' + timezone + '&elev=0&press=835&temp=10&dut1=0.0&deltat=64.797&refract=0.5667&ozone=0.3&pwv=1.5&aod=0.07637&ba=0.85&albedo=0.2&field=3&field=4&field=5&zip=0';
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("get", req, true);
-        xhttp.send();
-        return new Promise(function(resolve, reject) {
-            xhttp.onreadystatechange = function() {
-                if (xhttp.readyState == 4 && xhttp.status == 200) {
-                    var data = xhttp.responseText;
+        /*
+                var d = new Date();
+                var day = d.getDate();
+                var year = d.getFullYear();
+                var month = d.getMonth();
+                req = 'https://www.nrel.gov/midc/apps/sampa.pl?syear=' + year + '&smonth=' + month + '&sday=' + day + '&eyear=' + year + '&emonth=' + month + '&eday=' + day + '&step=60&stepunit=1&latitude=' + lat + '&longitude=' + lon + '&timezone=' + timezone + '&elev=0&press=835&temp=10&dut1=0.0&deltat=64.797&refract=0.5667&ozone=0.3&pwv=1.5&aod=0.07637&ba=0.85&albedo=0.2&field=3&field=4&field=5&zip=0';
+                var xhttp = new XMLHttpRequest();
+                xhttp.open("get", req, true);
+                xhttp.send();
+                return new Promise(function(resolve, reject) {
+                    xhttp.onreadystatechange = function() {
+                        if (xhttp.readyState == 4 && xhttp.status == 200) {
+                            var data = xhttp.responseText;
 
-                }
-            };
-        });
-        */
+                        }
+                    };
+                });
+                */
     }
 
 
@@ -174,10 +224,10 @@ angular.module('lunagrab.controllers', []).controller('AppCtrl', function($scope
     }
 
     function doUpdate() {
-        debug('orientation (compass, tilt): ' + $scope.orientation.compass + '   ' + $scope.orientation.tilt, 0);
+        debug(1,'orientation (compass, tilt): ', $scope.orientation.compass, $scope.orientation.tilt);
         var delta = getMoonDelta();
 
-        debug('delta (h, v): ' + delta.h + "   " + delta.v, 1);
+        debug(2, 'delta (h, v): ', delta.h , delta.v);
         positionArrow({ v: delta.v, h: delta.h });
         //debug('moon delta1 (h, v): '+delta.h+ "   "+delta.v, 4);
         positionMoon({ v: delta.v, h: delta.h });
@@ -194,12 +244,15 @@ angular.module('lunagrab.controllers', []).controller('AppCtrl', function($scope
 
     function positionArrow(delta) {
         var deg = getArrowAngle(delta);
-        debug('arrow angle: ' + deg, 2);
+        debug(4,'arrow angle: ', deg);
         $scope.arrowStyle = 'transform: rotate(' + deg + 'deg) translate(0, -80px)';
     }
 
-    function debug(txt, position) {
-        $scope.debug[position] = txt;
+    function debug(position, txt, var1, var2) {
+        $scope.debug[position] = [];
+        $scope.debug[position].push(txt);
+        $scope.debug[position].push(var1);
+        $scope.debug[position].push(var2);
 
         //document.getElementById("debug" + position).innerHTML = txt;
     }
--- www/js/app.js
+++ www/js/app.js
@@ -3,90 +3,39 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('lunagrab', ['ionic','lunagrab.controllers','lunagrab.services'])
-.run(function($ionicPlatform) {
-    var orientation;
-    var moon = {
-        compass: 0,
-        tilt: 90
-    }
-    $ionicPlatform.ready(function() {
-        return;
-       
-
-        //mock
-        return;
-        ezar.initializeVideoOverlay(
-            function() {
-                ezar.getBackCamera().start();
-
-                if (window.DeviceOrientationEvent) {
+angular.module('lunagrab', ['ionic', 'lunagrab.controllers', 'lunagrab.services'])
+    .run(function($ionicPlatform) {
+        var orientation;
+        var moon = {
+            compass: 0,
+            tilt: 90
+        }
+        $ionicPlatform.ready(function() {
+            ezar.initializeVideoOverlay(
+                function() {
+                    ezar.getBackCamera().start();
+                });
 
 
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-                    window.addEventListener('deviceorientation', function(eventData) {
-                        // gamma is the left-to-right tilt in degrees, where right is positive
-                        var tiltLR = Math.floor(eventData.gamma);
-
-                        // beta is the front-to-back tilt in degrees, where front is positive
-                        var tiltFB = Math.floor(eventData.beta);
-
-                        // alpha is the compass direction the device is facing in degrees
-                        var dir = Math.floor(eventData.alpha);
-
-                        // call our orientation event handler
-                        orientation.tilt = tiltFB;
-
-                        doUpdate();
-                        //debug(tiltLR, tiltFB, dir);
-                    }, false);
-
-
-                }
-
-
-
-                setInterval(function() {
-                    navigator.compass.getCurrentHeading(
-                        function(heading) {
-                            var hdng = Math.floor(heading.magneticHeading);
-                            orientation.compass = hdng;
-                            doUpdate();
-
-                        },
-                        function(err) {
-                            console.log("Compass error", err);
-                            debug(err, 0);
-                            ezar.getBackCamera().start();
-                        });
-                }, 500);
-
-
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
             }
-
-        )
-
-
-
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-            // Don't remove this line unless you know what you are doing. It stops the viewport
-            // from snapping when text inputs are focused. Ionic handles this internally for
-            // a much nicer keyboard experience.
-            cordova.plugins.Keyboard.disableScroll(true);
-        }
-        if (window.StatusBar) {
-            StatusBar.styleDefault();
-        }
-    });
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
 
 
 
 
-})
+    })
 
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -108,7 +57,7 @@ angular.module('lunagrab', ['ionic','lunagrab.controllers','lunagrab.services'])
         }
     })
 
-   
+
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/app/search');
 });
