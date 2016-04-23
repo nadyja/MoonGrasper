a29ced98ce972ea6c54a016eb51101d0a3bc00dc
nmietkiewicz <nadia.mietkiewicz@pracuj.pl>
2016-04-23 11:41:41 +0200

    added ezar compass as a base

--- www/js/index.js
+++ www/js/index.js
@@ -1,25 +1,9 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        this.updateHeading(0);
     },
     // Bind Event Listeners
     //
@@ -30,22 +14,71 @@ var app = {
     },
     // deviceready Event Handler
     //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // The scope of 'this' is the event. In order to call the 'doSnapshot'
+    // function, we must explicitly call 'app.doSnapshot(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        app.updateAppLayout();
+
+        ezar.initializeVideoOverlay(
+                function() {
+                    ezar.getBackCamera().start();
+
+                    //watchHeading() async update randomly stops on ios and android
+                    // use getCurrentHeading() as more reliable alternative 
+                    setInterval(function() {
+                        navigator.compass.getCurrentHeading(
+                            function(heading) { 
+                                var hdng = Math.floor(heading.magneticHeading);
+                                app.updateHeading(hdng);
+                            }, 
+                            function(err) {
+                                console.log("Compass error", err);
+                            });
+                    }, 500);
+ 
+                },
+                function(error) {
+                    console.log("Error initializing ezAR: " + error);
+                });
+
+        window.addEventListener("resize", function() {
+            app.updateAppLayout();
+        });
+        document.getElementById("snapbtn").addEventListener("click", function() {
+            app.doSnapshot();
+        });
+       
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+    updateAppLayout: function() {
+        document.getElementsByClassName("compass-outer")[0]
+                .style.height = window.innerHeight + "px";
+    },
+    updateHeading: function(hdng) {
+        // Set heading value
+        document.getElementById("heading-val")
+            .innerHTML = hdng;
+        // Rotate compass star
+        document.getElementById("compass-star")
+            .style.webkitTransform = "rotate(" + hdng + "deg)";
+    },
+    doSnapshot: function() {
+        //hide snapbtn so it will not be in snapshot image
+        document.getElementById("snapbtn").style.display = "none";
+        
+        //give dom chance to hide snapbtn before image capture
+        setTimeout( function() { 
+            ezar.snapshot(  
+                function(data) {
+                    alert("Snapshot complete.\nSee Gallery for image");
+                    document.getElementById("snapbtn").style.display = "block";
+                }, 
+                function(err) {
+                    alert("Error: " + err);
+                    document.getElementById("snapbtn").style.display = "block";
+                },
+                {"saveToPhotoAlbum":true}
+            )}, 10);
+    } 
 };
-
+    
 app.initialize();
