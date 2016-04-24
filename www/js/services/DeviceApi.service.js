987a70efad87328de9f775891660974c0bfcfc40
Marcin <dlugopix@gmail.com>
2016-04-24 12:34:35 +0200

    Added real location (GPS)

--- www/js/services/DeviceApi.service.js
+++ www/js/services/DeviceApi.service.js
@@ -1,86 +1,92 @@
-angular.module('MoonGrasper').factory('DeviceApi', function($http, $q) {
-    return {
-        
-        isDebug: function() {
-            //return true;
-            return false;
-            //helper for debugging with ionic serve
+angular.module('MoonGrasper').factory('DeviceApi', function ($http, $q) {
+  return {
 
-        },
-        initCameraBackground: function() {
-           
-        },
-        initTiltListner: function(callback) {
-            if (window.DeviceOrientationEvent) {
-                window.addEventListener('deviceorientation', function(eventData) {
-                    // gamma is the left-to-right tilt in degrees, where right is positive
-                    var tiltLR = eventData.gamma;
-                    // beta is the front-to-back tilt in degrees, where front is positive
-                    var tiltFB = eventData.beta;
-                    // alpha is the compass direction the device is facing in degrees
-                    var dir = eventData.alpha;
+    isDebug: function () {
+      //return true;
+      return false;
+      //helper for debugging with ionic serve
 
-                    callback(tiltFB);
-                }, false);
+    },
+    initCameraBackground: function () {
+
+    },
+    initTiltListner: function (callback) {
+      if (window.DeviceOrientationEvent) {
+        window.addEventListener('deviceorientation', function (eventData) {
+          // gamma is the left-to-right tilt in degrees, where right is positive
+          var tiltLR = eventData.gamma;
+          // beta is the front-to-back tilt in degrees, where front is positive
+          var tiltFB = eventData.beta;
+          // alpha is the compass direction the device is facing in degrees
+          var dir = eventData.alpha;
+
+          callback(tiltFB);
+        }, false);
 
 
-            }
+      }
+    },
+    initCompassListner: function (callback) {
+      var compass = {
+        onSuccess: function (heading) {
+          var hdng = heading.magneticHeading;
+          callback(hdng);
+          neverUsedCompass = false;
         },
-        initCompassListner: function(callback) {
-            var compass = {
-                onSuccess: function(heading) {
-                    var hdng = heading.magneticHeading;
-                    callback(hdng);
-                    neverUsedCompass=false;
-                },
-                onError: function(compassError) {
-                    console.log("Compass error", err);
-                    debug(5, err);
-                    ezar.getBackCamera().start();
-                },
-                options: {
-                    frequency: 100
-                }
-            }
-            setInterval(function() {
-             navigator.compass.getCurrentHeading(compass.onSuccess, compass.onError);
-              }, 10);
+        onError: function (compassError) {
+          console.log("Compass error", err);
+          debug(5, err);
+          ezar.getBackCamera().start();
         },
-        getAngleOfView: function() {
-            return { v: 75, h: 75 }; //mock
-            //TODO:  find a way to get real camera angle of view
-            /*
-              var p = camera.getParameters();
-              var thetaV = Math.toRadians(p.getVerticalViewAngle());
-              var thetaH = Math.toRadians(p.getHorizontalViewAngle());
-              var theta = {
-                  v: thetaV,
-                  h: thetaH,
-              }
-              return theta;
-            */
+        options: {
+          frequency: 100
+        }
+      }
+      setInterval(function () {
+        navigator.compass.getCurrentHeading(compass.onSuccess, compass.onError);
+      }, 10);
+    },
+    getAngleOfView: function () {
+      return {v: 75, h: 75}; //mock
+      //TODO:  find a way to get real camera angle of view
+      /*
+       var p = camera.getParameters();
+       var thetaV = Math.toRadians(p.getVerticalViewAngle());
+       var thetaH = Math.toRadians(p.getHorizontalViewAngle());
+       var theta = {
+       v: thetaV,
+       h: thetaH,
+       }
+       return theta;
+       */
+    },
+    getDisplayResolution: function () {
+      return {v: 360, h: 640}; //mock
+      //TODO:  find out why this doesn't work
+      var display = getWindowManager().getDefaultDisplay();
+      var size = new Point();
+      display.getSize(size);
+      var resolution = {
+        v: size.y,
+        h: size.x
+      }
+      return resolution;
+    },
+    getCoordinatesAndTimezone: function (callback) {
+      var location = navigator.geolocation.getCurrentPosition(
+        function (data) {
+          callback({
+            lat: data.coords.latitude,
+            lon: data.coords.longitude,
+            timezone: 2
+          })
         },
-        getDisplayResolution: function() {
-            return { v: 360, h: 640 }; //mock
-            //TODO:  find out why this doesn't work
-            var display = getWindowManager().getDefaultDisplay();
-            var size = new Point();
-            display.getSize(size);
-            var resolution = {
-                v: size.y,
-                h: size.x
-            }
-            return resolution;
-        },
-        getCoordinatesAndTimezone: function() {
-            // TODO: find the real ones
-            return {
-                lat: 52,
-                lon: 14,
-                timezone: 2
-            };
-        },
+        function (err) {
+          console.log("error")
+        }
+      );
+    },
 
-    }
+  }
 
 });
